7943610f16291a4460712356e73f4a19b10442ce
anandhu <anandhureji33@gmail.com>
2023-09-28 20:25:06 +0530

    Working on navbar

---
+++ components/index.ts
@@ -0,0 +1,13 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Image from "next/image";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export{
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer
+
+}
